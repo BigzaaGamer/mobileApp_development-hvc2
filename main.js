9fcc7d4c6239c13441178afb1bdffde7d13a3064
Konkamon Sion <bigkonkamon@gmail.com>
2021-12-11 10:12:05 +0700

    Updated to workshop9

--- main.js
+++ main.js
@@ -4,10 +4,21 @@ const cityApi = require('./workshop9/api/city');
 const coordinateApi = require('./workshop9/api/coordinate');
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', './workshop9/views');
+app.set('views', './static/views');
 app.use(express.json());
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/static'));
 const PORT = process.env.PORT || 3000;
+
+app.get('/workshop8', function(req, res){
+    fs.readFile('./workshop8/data.json', (err, data) =>{
+        const dataList = JSON.parse(data);
+        if(err){
+            res.status(400).send("ERROR! List not found!");
+        } else {
+            res.render('konkamon_template',{lists:dataList});
+        }
+    });
+});
 
 app.get('/workshop9', function (req, res) {
     fs.readFile('./workshop9/th-city.json', (err, data) => {
@@ -31,6 +42,29 @@ app.get('/workshop9/api/city', function (req, res) {
         });
     } else {
         const resultData = cityApi(city, country)
+            .then((result) => {
+                const data = (result.data);
+                res.status(200).render('forecast', {
+                    forcastObj: result.data
+                });
+            })
+            .catch((err) => {
+                res.status(400).render('notfound', {
+                    forcastObj: err
+                });
+            });
+    }
+});
+app.get('/workshop9/api/coordinate', function (req, res) {
+    const longitude = req.query.lon !== "" ? req.query.lon : "0";
+    const latitude = req.query.lat !== "" ? req.query.lat : "0";
+    if (longitude == null || latitude == null) {
+        const err = 'Error';
+        res.status(400).render('notfound', {
+            forcastObj: err
+        });
+    } else {
+        const resultData = coordinateApi(longitude, latitude)
             .then((result) => {
                 const data = (result.data);
                 res.status(200).render('forecast', {
